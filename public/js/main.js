2d58fac0aee45499016ee3e6bb93a983c83eec6b
Cathreena-Paul <cathreenapaul@gmail.com>
2025-04-10 23:54:59 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
+document.querySelectorAll(".dropdown-content a").forEach(link => 
+    link.addEventListener("click", () => {
+        document.querySelector(".active")?.classList.remove("active");  
+        link.classList.add("active");
+    })
+);
+
 import { API_KEY } from './apikey.js';
 
 let api_key = API_KEY.key;
