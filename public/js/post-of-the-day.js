7dd9d9c301c3d5a5d13bf8a100f21d49cf22b58f
virendraramsuchit <virendraramsuchit@gmail.com>
2025-04-12 01:04:49 -0400

    js landscape changes

--- public/js/post-of-the-day.js
+++ public/js/post-of-the-day.js
@@ -22,33 +22,30 @@ let data = await getPostOfDay();
 
 function makeCard(info){
 
-    let result = document.querySelector('.post-content');
+    let result = document.querySelector('.content');
 
     let html = '';
 
     html += `
     
-            <div class="post-body">
-            <h1>${info.title}</h1>
-            <p class="date">DATE: ${info.date}</p>
-            <p>${info.explanation}</p>
-            <img class="post-image" src="${info.hdurl}" alt="Post Image">
-            
-
-            <div class="post-footer">
-                <div class="reaction-buttons">
-                    <!--onclick activates firebase-->
-                    <button onclick="">&#x1F44D; Like</button>
-                    <button onclick="">&#x1F4A1; Comment</button>
-                    <button  onclick="">&#x1F516; Save</button>
-                </div>
-
-                <div class="reaction-buttons">
-                    <button  onclick="" >View Comments</button>
-                    <!--onclick would trigger js and popup modal with comments-->
+        <div class="post-content">
+                <img class="post-image" src="${info.hdurl}" alt="Post Image">
+                <div class="post-body">
+                    <h1>${info.title}</h1>
+                    <p class="date" >${info.date}</p>
+                    <p>${info.explanation}</p>
+                    <div class="post-footer">
+                        <div class="reaction-buttons">
+                            <button onclick="">&#x1F44D; Like</button>
+                            <button onclick="">&#x1F4A1; Comment</button>
+                            <button onclick="">&#x1F516; Save</button>
+                        </div>
+                        <div class="reaction-buttons">
+                            <button onclick="">View Comments</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
     
     `;
 
