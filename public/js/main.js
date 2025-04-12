dca552ac50477c10ab1bb53c90d96b54ed6117ce
virendraramsuchit <virendraramsuchit@gmail.com>
2025-04-11 23:28:29 -0400

    Some added js functions

--- public/js/main.js
+++ public/js/main.js
@@ -9,23 +9,63 @@ import { API_KEY } from './apikey.js';
 
 let api_key = API_KEY.key;
 
-//Test to make sure hidden API key works 
-async function getData (){
+async function getThisWeekInSpace (){
 
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?&api_key=${api_key}`);
-    let data = await response.json();
-    return data;
+    const today = new Date();
+    const endDate = today.toISOString().split('T')[0];
 
-}
+    const startDate = new Date();
+    startDate.setDate(today.getDate() - 7);
+    const startDateStr = startDate.toISOString().split('T')[0];
 
-let state = await getData();
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?&api_key=${api_key}&start_date=${startDateStr}&end_date=${endDate}`);
+    let thisWeekData = await response.json();
 
-function showData(info){
-
-    console.log(`Title:${info.title} `);
-    console.log(`Date:${info.date}`);
+    return thisWeekData;
 
 }
 
-showData(state);
+let data = await getThisWeekInSpace ();
+
+function makePost(info){
+
+    let result = document.querySelector('#results');
+
+    let html = '';
+
+    for(let rec of info){
+
+        html += `
+        
+        <div class="post-content">
+          
+            <div class="post-body">
+                <h1>${rec.title}</h1>
+                <p>${rec.explanation}</p>
+                <img class="post-image" src="${rec.hdurl}" alt="Post Image">
+                </div>
+    
+                <div class="post-footer">
+                    <div class="reaction-buttons">
+                        <!--onclick activates firebase-->
+                        <button onclick="">&#x1F44D; Like</button>
+                        <button onclick="">&#x1F4A1; Comment</button>
+                        <button  onclick="">&#x1F516; Save</button>
+                    </div>
+
+                    <div class="reaction-buttons">
+                        <button  onclick="" >View Comments</button>
+                        <!--onclick would trigger js and popup modal with comments-->
+                    </div>
+                </div>
+        </div>
+
+        `;
+    }
+
+    result.innerHTML = html;
+
+}
+
+makePost(data);
 
--- public/js/post-of-the-day.js
+++ public/js/post-of-the-day.js
@@ -5,3 +5,54 @@ document.querySelectorAll(".dropdown-content a").forEach(link =>
     })
 );
 
+import { API_KEY } from './apikey.js';
+
+let api_key = API_KEY.key;
+
+//1. APOD Section
+async function getPostOfDay(){
+
+    let response = await fetch(`https://api.nasa.gov/planetary/apod?&api_key=${api_key}`);
+    let todayData = await response.json();
+
+    return todayData;
+}
+
+let data = await getPostOfDay();
+
+function makeCard(info){
+
+    let result = document.querySelector('.post-content');
+
+    let html = '';
+
+    html += `
+    
+            <div class="post-body">
+            <h1>${info.title}</h1>
+            <p class="date">DATE: ${info.date}</p>
+            <p>${info.explanation}</p>
+            <img class="post-image" src="${info.hdurl}" alt="Post Image">
+            
+
+            <div class="post-footer">
+                <div class="reaction-buttons">
+                    <!--onclick activates firebase-->
+                    <button onclick="">&#x1F44D; Like</button>
+                    <button onclick="">&#x1F4A1; Comment</button>
+                    <button  onclick="">&#x1F516; Save</button>
+                </div>
+
+                <div class="reaction-buttons">
+                    <button  onclick="" >View Comments</button>
+                    <!--onclick would trigger js and popup modal with comments-->
+                </div>
+            </div>
+        </div>
+    
+    `;
+
+    result.innerHTML = html;
+}
+
+makeCard(data);
